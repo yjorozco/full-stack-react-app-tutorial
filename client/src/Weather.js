e161557656d3f50b9a6d68c11dbdb84a1f26035d
Yamil Orozco <yamiloro@gmail.com>
2019-10-05 03:36:46 -0430

    make it better

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -18,28 +18,28 @@ export default function Weather(props) {
                 <br />
                 <span>{data.data.weather[0].main}</span>&nbsp;
                 <br />
-                <span>{Math.floor(data.data.main.temp)}&deg;F</span>
+                <span>{Math.floor(parseFloat(data.data.main.temp))}&deg;F</span>
                 <Table>
                     <tbody>
                         <tr>
                             <td>wind</td>
-                            <td>{Math.floor(data.data.wind.speed)}</td>
+                            <td>{Math.floor(parseFloat(data.data.wind.speed))}</td>
                         </tr>
                         <tr>
                             <td>Pressure</td>
-                            <td>{Math.floor(data.data.wind.pressure)} hPa</td>
+                            <td>{Math.floor(parseFloat(data.data.wind.pressure))} hPa</td>
                         </tr>
                         <tr>
                             <td>Humidity</td>
-                            <td>{Math.floor(data.data.wind.humidity)} %</td>
+                            <td>{Math.floor(parseFloat(data.data.wind.humidity))} %</td>
                         </tr>
                         <tr>
                             <td>Min Temp</td>
-                            <td>{Math.floor(data.data.wind.temp_min)} &deg;F</td>
+                            <td>{Math.floor(parseFloat(data.data.wind.temp_min))} &deg;F</td>
                         </tr>
                         <tr>
                             <td>Max Temp</td>
-                            <td>{Math.floor(data.data.wind.temp_max)} &deg;F</td>
+                            <td>{Math.floor(parseFloat(data.data.wind.temp_max))} &deg;F</td>
                         </tr>
                     </tbody>
                 </Table>
