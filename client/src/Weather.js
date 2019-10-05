3012dda429eb102bf2ee73a6a2062d98d145cb0d
Yamil Orozco <yamiloro@gmail.com>
2019-10-05 03:48:37 -0430

    make it better

--- client/src/Weather.js
+++ client/src/Weather.js
@@ -9,7 +9,7 @@ export default function Weather(props) {
         return (<div></div>);
    
     const img = data.data.weather[0].icon;
-    console.log(data);
+
     return (
         <Row className="weathe">
             <Col sm="12" md={{size:4, offset:4}}>
@@ -18,28 +18,28 @@ export default function Weather(props) {
                 <br />
                 <span>{data.data.weather[0].main}</span>&nbsp;
                 <br />
-                <span>{Math.floor(Number.parseFloat(data.data.main.temp))}&deg;F</span>
+                <span>{Math.floor(parseFloat(data.data.main.temp))}&deg;F</span>
                 <Table>
                     <tbody>
                         <tr>
                             <td>wind</td>
-                            <td>{Math.floor(Number.parseFloat(data.data.wind.speed))}</td>
+                            <td>{Math.floor(data.data.main.speed)}</td>
                         </tr>
                         <tr>
                             <td>Pressure</td>
-                            <td>{Math.floor(Number.parseFloat(data.data.wind.pressure))} hPa</td>
+                            <td>{Math.floor(data.data.main.pressure)} hPa</td>
                         </tr>
                         <tr>
                             <td>Humidity</td>
-                            <td>{Math.floor(Number.parseFloat(data.data.wind.humidity))} %</td>
+                            <td>{Math.floor(data.data.main.humidity)} %</td>
                         </tr>
                         <tr>
                             <td>Min Temp</td>
-                            <td>{Math.floor(Number.parseFloat(data.data.wind.temp_min))} &deg;F</td>
+                            <td>{Math.floor(data.data.main.temp_min)} &deg;F</td>
                         </tr>
                         <tr>
                             <td>Max Temp</td>
-                            <td>{Math.floor(Number.parseFloat(data.data.wind.temp_max))} &deg;F</td>
+                            <td>{Math.floor(data.data.main.temp_max)} &deg;F</td>
                         </tr>
                     </tbody>
                 </Table>
